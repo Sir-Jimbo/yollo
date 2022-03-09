49f72353f274eff03bbeb62da753f2e748fc834a
Alexey Golodenko <alexey.golodenko@gmail.com>
2022-03-09 23:56:59 +0200

    fix_bags_4

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ const burger = document.querySelector('.header__burger');
 const body = document.querySelector('.body')
 const menu = document.querySelector('.header__menu');
 const close = document.querySelector('.header__close');
-const link = document.querySelector('.header__menu-link')
+
 burger.addEventListener('click', openMenu);
 close.addEventListener('click', closeMenu);
-link.addEventListener('click', closeLink);
+
 
 function openMenu() {
 
@@ -25,13 +25,6 @@ function closeMenu() {
    burger.classList.remove('close')
 }
 
-function closeLink() {
-   const menu = document.querySelector('.header__menu');
-   body.classList.remove('active');
-   menu.classList.remove('open');
-   close.classList.remove('active');
-   burger.classList.remove('close')
-}
 
 const swiper = new Swiper('.swiper', {
 
@@ -84,3 +77,46 @@ let tab = function () {
 }
 
 tab()
+const links = document.querySelectorAll('a[href*="#"]')
+links.forEach(function (item) {
+   item.addEventListener('click', function (e) {
+      e.preventDefault();
+      body.classList.remove('active');
+      menu.classList.remove('open');
+      close.classList.remove('active');
+      burger.classList.remove('close')
+   })
+})
+// собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+   animationTime = 1500,
+   framesCount = 30;
+
+anchors.forEach(function (item) {
+   // каждому якорю присваиваем обработчик события
+   item.addEventListener('click', function (e) {
+      // убираем стандартное поведение
+      e.preventDefault();
+
+      // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+      let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+      // запускаем интервал, в котором
+      let scroller = setInterval(function () {
+         // считаем на сколько скроллить за 1 такт
+         let scrollBy = coordY / framesCount;
+
+         // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+         // и дно страницы не достигнуто
+         if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+            // то скроллим на к-во пикселей, которое соответствует одному такту
+            window.scrollBy(0, scrollBy);
+         } else {
+            // иначе добираемся до элемента и выходим из интервала
+            window.scrollTo(0, coordY);
+            clearInterval(scroller);
+         }
+         // время интервала равняется частному от времени анимации и к-ва кадров
+      }, animationTime / framesCount);
+   });
+});
